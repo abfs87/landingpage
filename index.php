1d2387c33e5dd67dcd50aeba3eee2d8d6d032f42
abfs87 <reversedrug.arc@gmail.com>
2015-11-05 16:24:03 +0700

    Start first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" name="viewport" content="width=device-with, initial-scale=1.0">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Senja Merona Travel</title>
+    </head>
+    <body>
+        <header>
+            <div class="navbar navbar-default navbar-static-top">
+               <div class="container-fluid">
+                  <div class="navbar-header">
+                   <a class="navbar-brand" href="#">Senja Merona Travel</a>
+                   <button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+                       <span class="icon-bar"></span>
+                       <span class="icon-bar"></span>
+                       <span class="icon-bar"></span>
+                   </button>
+                   </div>
+                   <div class="collapse navbar-collapse navHeaderCollapse">
+                       <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#">Beranda</a></li>
+                            <li><a href="#">Profile</a></li>
+                            <li><a href="#">Layanan</a></li>
+                            <li><a href="#">Kontak</a></li>
+                       </ul>
+                   </div> 
+                </div>
+            </div>
+        </header>
+
+        <footer>
+           <div class="navbar navbar-default navbar-fixed-bottom ">
+                <div class="container">
+                    <p class="navbar-text">&copy;Senja Merona Travel Est.2015</p>
+                </div>
+           </div>
+            
+        </footer>
+        <script src="js/jquery-2.1.4.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/script.js"></script>
+    </body>
+    
+</html>
